b5e14ec917748163c9a25ec3b4406e1b35836682
abhishekkashyapsudo <abhishek.abhikashyap@gmail.com>
2021-06-25 13:13:56 +0530

    Adding test message

---
+++ Sellers/Test/nagp/directservice/sellers/models/CategoryTest.java
@@ -0,0 +1,15 @@
+package nagp.directservice.sellers.models;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CategoryTest {
+
+	
+	@Test
+	public void test() {
+		assertEquals( Category.GROOMING.name(), "GROOMING");
+	}
+	
+}
